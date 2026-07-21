2f454156c7cb6ac6de02dfe43ce3b69702a2dc6a
Jevon Cochran <jevon@securebags.com>
2026-07-21 19:13:26 -0400

    put videos on website

---
+++ src/lib/data/gallery.ts
@@ -0,0 +1,18 @@
+export type GalleryVideo = {
+  id: string;
+  title: string;
+  playbackId: string;
+};
+
+export const videos: GalleryVideo[] = [
+  {
+    id: "1",
+    title: "Lutz Home Renovation 1",
+    playbackId: "UEJ8bOrZihGk4WEnXQNLM9TosUC5KLbnQkrRuXDePr4",
+  },
+  {
+    id: "2",
+    title: "Lutz Home Renovation 2",
+    playbackId: "cgQ16stpqzEqWFlg474ZaTN2x15NVUkBZicZ6rQrqRQ",
+  },
+];
